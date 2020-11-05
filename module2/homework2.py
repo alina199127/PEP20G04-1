88398ff4ea95682112a346cd7fd84957d81197a9
alina199127 <71280025+alina199127@users.noreply.github.com>
2020-11-05 16:01:55 +0100

    Update homework2.py

--- module2/homework2.py
+++ module2/homework2.py
@@ -1,3 +1,6 @@
 """Homework 2 - needs to be presented before exam day"""
 
 # Prove "and" operation takes precedence over "or" operation by setting parentheses in (False or False and True or True)
+print(False or (False and True) or True)
+print(False or False) and (True or True)
+print(False or False and True or True)
