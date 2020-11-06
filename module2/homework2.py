fb6b562d1d44f69b2e07e0d9d6fac00c48f433d8
alina199127 <71280025+alina199127@users.noreply.github.com>
2020-11-06 15:55:55 +0100

    Update homework2.py

--- module2/homework2.py
+++ module2/homework2.py
@@ -1,4 +1,3 @@
 # Prove "and" operation takes precedence over "or" operation
-print(False or (False and True) or True)
-print(False or False) and (True or True)
-print(False or False and True or True)
+print((False or (False and True) or True)==(False or False and True or True))
+print((False or False) and (True or True)==(False or False and True or True))
